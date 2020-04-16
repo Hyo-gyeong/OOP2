d1d6200f30d4639665fa8340367fb5e717f8d4e0
Hyo-gyeong <50789692+Hyo-gyeong@users.noreply.github.com>
2020-04-17 01:07:27 +0900

    객지2과제/Math.random()

--- Week2_3.java
+++ Week2_3.java
@@ -7,7 +7,7 @@ public class Week2_3 {
 		System.out.println("random() À¸·Î ¹ß»ýÇÑ ¼ö : ");
 		for (int i = 0; i <100; i++) {
 			double randNum = Math.random(); //0 <= randNum < 1, 0을 포함함!
-			int rn = (int)(randNum * 100) + 1;//1을 더해줘야 최소수가 1이됨
+			int rn = (int)(randNum * 100) + 1;//1을 더해줘야 1~100출력 가능
 			sum += rn;
 			
 			System.out.print(rn + "\t");
