c0f2f273034f303882b643c707351b9f8fd3c427
Hyo-gyeong <50789692+Hyo-gyeong@users.noreply.github.com>
2020-04-17 14:11:16 +0900

    객지2과제/객체배열사용

--- Week2_6/Week2_6.java
+++ Week2_6/Week2_6.java
@@ -7,21 +7,15 @@ public class Week2_6 {
 		Scanner sc = new Scanner(System.in);
 		Phone[] info = new Phone[2];
 		
-		System.out.print("이름, 전화번호를 입력하세요. >> ");
-		String name0 = sc.next();
-		String pn0 = sc.next();
-		
-		info[0] = new Phone(name0, pn0);
-		
-		System.out.println(info[0].getName() + "의 번호는 "+ info[0].getTel());
-		
-		System.out.print("이름, 전화번호를 입력하세요. >> ");
-		String name1 = sc.next();
-		String pn1 = sc.next();
-		
-		info[1] = new Phone(name1, pn1);
-		
-		System.out.println(info[1].getName() + "의 번호는 "+ info[1].getTel());
+		for (int i = 0; i < 2; i++) {
+			System.out.print("이름, 전화번호를 입력하세요. >> ");
+			String name = sc.next();
+			String pn = sc.next();
+			
+			info[i] = new Phone(name, pn);
+			
+			System.out.println(info[i].getName() + "의 번호는 "+ info[i].getTel());
+		}
 		
 		sc.close();
 		
